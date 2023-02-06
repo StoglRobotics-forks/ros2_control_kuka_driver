1fc61a62c2d641d19f93bfdd78cb14429185774c
Manuel M <mamueluth@gmail.com>
2023-02-06 10:22:03 +0100

    add both to out

--- launch/position.launch.py
+++ launch/position.launch.py
@@ -34,10 +34,7 @@ def generate_launch_description():
             package='controller_manager',
             executable='ros2_control_node',
             parameters=[robot_description, robot_forward_controller],
-            output={
-            'stdout': 'screen',
-            'stderr': 'screen',
-            },
+            output="both",
         ),
         Node(
             package='robot_state_publisher',
